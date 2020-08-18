7bdbef9b8e0cb130c11385a6707caa30d3e0a57b
olivier delfosse <olividelfosse@gmail.com>
2020-08-18 14:45:16 +0200

    start filter vehicles

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -4,7 +4,12 @@ class VehiclesController < ApplicationController
   end
 
   def index
-    @vehicles = Vehicle.all
+
+    min_price = 50
+    max_price = 15000
+    puts params
+    # raise
+    @vehicles = Vehicle.where("daily_price >= (?) AND daily_price < (?)", min_price, max_price)
 
   end
 
