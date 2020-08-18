9b49b1bfad474e58a3245f20c738998ad9ba0672
olivier delfosse <olividelfosse@gmail.com>
2020-08-18 16:46:25 +0200

    add vehicles index and filter

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -4,11 +4,16 @@ class VehiclesController < ApplicationController
   end
 
   def index
-    min_price = 50
-    max_price = 15000
-    puts params
-    # raise
-    @vehicles = Vehicle.where("daily_price >= (?) AND daily_price < (?)", min_price, max_price)
+    # if params[:min_price]
+    min_price = (params[:min_price] != "" && params[:min_price] !=  nil) ? params[:min_price] : 1
+    max_price = (params[:max_price] != "" && params[:max_price] !=  nil) ? params[:max_price] : 2000000
+    category = params[:category] || nil
+
+    if category
+      @vehicles = Vehicle.where("category = LOWER((?)) AND daily_price >= (?) AND daily_price < (?)", category, min_price, max_price)
+    else
+      @vehicles = Vehicle.where("daily_price >= (?) AND daily_price < (?)", min_price, max_price)
+    end
   end
 
   def show
