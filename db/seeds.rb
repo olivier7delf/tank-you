fd52c94746661f6ffa6e6825d65ed34faa7ded07
Theotime Lameyre <theotime.lameyre@gmail.com>
2020-08-21 16:15:29 +0200

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -137,7 +137,7 @@ if mode_prod
     name: "Sous-Marin Nucléaire",
     category: "mer",
     daily_price: '99000',
-    description: "_",
+    description: "À la poursuite d'Octobre rouge est un film américain réalisé par John McTiernan, sorti en 1990. Il s'agit de l'adaptation cinématographique du roman Octobre rouge du romancier Tom Clancy, paru en 1984.",
     capacity: '52'
   )
   file0 = URI.open('https://www.meretmarine.com/objets/4231.jpg')
@@ -148,7 +148,7 @@ if mode_prod
     name: "La Santa-Maria",
     category: "mer",
     daily_price: '600',
-    description: "_",
+    description: "Pour vous détendre du code, allez ramer avec vos potes",
     capacity: '25'
   )
   file0 = URI.open('https://www.actu-direct.fr/rep/rep_article/2018-07-12_080546_DSCN0282.JPG')
@@ -159,7 +159,7 @@ if mode_prod
     name: "Char Leclerc",
     category: "terre",
     daily_price: '15000',
-    description: "_",
+    description: "Charmant et canon, avec un grand coffre, idéal pour faire ses courses au supermarché",
     capacity: '3'
   )
   file0 = URI.open('https://www.guer-coetquidan-broceliande.fr/bisto/coet/campchars/leclerc_3.jpg')
@@ -170,7 +170,7 @@ if mode_prod
     name: "Char Panzer",
     category: "terre",
     daily_price: '10000',
-    description: "_",
+    description: "Parfait pour la course de chars à voile sur les plages de Normandie",
     capacity: '4'
   )
   file0 = URI.open('https://upload.wikimedia.org/wikipedia/commons/a/ac/PzIV.Saumur.000a5s6s.jpg')
@@ -181,7 +181,7 @@ if mode_prod
     name: "Spitfire",
     category: "air",
     daily_price: '1000',
-    description: "_",
+    description: "Le Supermarine Spitfire (en anglais cracheur de feu, mais aussi au sens figuré soupe au lait ou mégère, d'où dragon également) est l'un des chasseurs monoplaces les plus utilisés par la RAF et par les Alliés pendant la Seconde Guerre mondiale. Il donna lieu à une diversification et à une multiplicité de versions jamais atteinte auparavant dans l'histoire de l'aviation.",
     capacity: '2'
   )
   file0 = URI.open('https://cdn-3d.niceshops.com/upload/image/product/large/default/revell-supermarine-spitfire-mkiia-1-pcs-304157-fr.jpg')
@@ -192,7 +192,7 @@ if mode_prod
     name: "HoverBoard Green Goblin",
     category: "air",
     daily_price: '12000',
-    description: "_",
+    description: "Plus un pigeon n'osera vous viser dans la rue",
     capacity: '1'
   )
   file0 = URI.open('https://bradenkelley.com/wp-content/uploads/2018/03/Green-Goblin-Flying-1024x844.jpg')
@@ -203,7 +203,7 @@ if mode_prod
     name: "Rafale",
     category: "air",
     daily_price: '30000',
-    description: "_",
+    description: "L'expert du balle trape",
     capacity: '2'
   )
   file0 = URI.open('https://images.dassault-aviation.com/f_auto,q_auto,g_center,dpr_auto/wp-auto-upload/1/files/2017/10/DA00016390_Si.jpg')
@@ -214,7 +214,7 @@ if mode_prod
     name: "Nautilus",
     category: "mer",
     daily_price: '5000',
-    description: "_",
+    description: "Le Nautilus macromphalus (Sowerby, 1849) est une espèce de nautiles endémique de la région entourant la Nouvelle-Calédonie. Chez cette espèce le centre de la volute présente un creux caractéristique, souvent colonisé par des crustacés parasites. C'est la plus petite espèce de nautile  : sa coquille mesure environ 16 cm, tandis que le plus gros spécimen connu mesurait 18 cm.",
     capacity: '40'
   )
   file0 = URI.open('https://i.pinimg.com/originals/a4/f1/1e/a4f11e009c003e76e8043fc9cf7ab6d8.jpg')
@@ -225,7 +225,7 @@ if mode_prod
     name: "Porte-Avion",
     category: "mer",
     daily_price: '200000',
-    description: "_",
+    description: "Parfait pour de la plaisance entre potes",
     capacity: '2000'
   )
   file0 = URI.open('https://www.meretmarine.com/sites/default/files/styles/mem_1000/public/new_objets_drupal/cdg2_2.jpg')
@@ -236,29 +236,29 @@ if mode_prod
     name: "Executor class Super Star Destroyer",
     category: "espace",
     daily_price: '200000',
-    description: "_",
+    description: "Toujours à la recherche d'un garage",
     capacity: '2000'
   )
   file0 = URI.open('https://vignette.wikia.nocookie.net/starwars/images/3/30/Executor_BF2.png/revision/latest?cb=20180903230846')
   vehicle.photos.attach(io: file0, filename: 'Superdestroyer.jpg', content_type: 'image/png')
   vehicle.save!
-  puts "Creating Bubblebee..."
+  puts "Creating Bumblebee..."
   vehicle = Vehicle.new(
-    name: "Bubblebee",
+    name: "Bumblebee",
     category: "terre",
     daily_price: '54000',
-    description: "_",
+    description: "🐝 Bumblebee est un robot dans l'univers de fiction Transformers. Il est membre des Autobots, un groupe de robots extraterrestres sentients capables de se transformer en divers véhicules. Dans la plupart des versions, Bumblebee est une petite Volkswagen Coccinelle jaune, mais dans les films d'action, il apparaît comme une muscle car (voiture sportive américaine) jaune et noire. Le personnage doit son nom au bourdon, une abeille à rayures noires et jaunes. Bumblebee apparaît dans la plupart des séries et devient le principal protagoniste de Transformers: Robots in Disguise, Bumblebee et Transformers: Cyberverse.",
     capacity: '5'
   )
   file0 = URI.open('https://images7.alphacoders.com/517/517778.jpg')
   vehicle.photos.attach(io: file0, filename: 'Bubblebee.jpg', content_type: 'image/jpg')
   vehicle.save!
-  puts "Creating Etoile Noire..."
+  puts "Creating Etoile De La Mort..."
   vehicle = Vehicle.new(
-    name: "Etoile Noire",
+    name: "Etoile De La Mort",
     category: "espace",
     daily_price: '35400',
-    description: "_",
+    description: "Station spatiale sidérale mobile de forme sphérique et de la taille d'une petite lune, présente dans la saga Star Wars: pour ceux qui veulent décrocher la lune",
     capacity: '1500'
   )
   file0 = URI.open('https://media.anakinworld.com/uploads/entries/original/vaisseau-etoile-noire-i.jpg')
@@ -269,8 +269,8 @@ if mode_prod
     name: "Tigre",
     category: "air",
     daily_price: '6000',
-    description: "_",
-    capacity: '1'
+    description: "Pour une balade romantique au moyenne orient",
+    capacity: '2'
   )
   file0 = URI.open('https://www.helicopassion.com/images/TIGR/Espagne/PRADA89h.jpg')
   vehicle.photos.attach(io: file0, filename: 'Tigre.jpg', content_type: 'image/jpg')
@@ -280,10 +280,9 @@ if mode_prod
     name: "Cheval Blanc d'Henri IV",
     category: "terre",
     daily_price: '20',
-    description: "_",
+    description: "Monture de légende sans corne 🦄",
     capacity: '1'
   )
-
   file0 = URI.open('https://p6.storage.canalblog.com/68/62/1309854/106788352_o.jpg')
   vehicle.photos.attach(io: file0, filename: 'ChevalBlanc.jpg', content_type: 'image/jpg')
   vehicle.save!
@@ -292,7 +291,7 @@ if mode_prod
     name: "Char Tesla",
     category: "terre",
     daily_price: '3000',
-    description: "_",
+    description: "⚡️ Véhicule de loisir pour se tenir au courant ⚡️",
     capacity: '5'
   )
   file0 = URI.open('https://alerte-rouge.e-monsite.com/medias/album/images/38630828ps3-command-et-conquer-red-alert-3-1212412153-9-1-jpg.jpg')
